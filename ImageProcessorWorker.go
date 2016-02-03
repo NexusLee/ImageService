84225825b284571d53b94cc60ebdf1004a2582de
Jakub Martin <jakub.wit.martin@gmail.com>
2016-02-03 23:03:58 +0100

    Process count control.

--- ImageProcessorWorker.go
+++ ImageProcessorWorker.go
@@ -11,7 +11,7 @@ import (
 func startProcessor(workToDo chan string, finishedWorkMap *map[string]bool) {
 	var workId string
 	finishedWorkCommunicator := make(chan string)
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 20; i++ {
 		go startImageProcessorWorker(workToDo, finishedWorkCommunicator)
 	}
 	for {
