f0e0d1d3c1431934c0bb349b0e599320f3bf7157
Jakub Martin <jakub.wit.martin@gmail.com>
2016-02-03 23:08:52 +0100

    Process count control.

--- ImageFileService.go
+++ ImageFileService.go
@@ -2,7 +2,7 @@ package ImageFileMicroservices
 
 func RunService() {
 	workToDo := make(chan string)
-	finishedWorkMap := make(map[string]bool)
+	finishedWorkMap := make(map[string]bool, 1024)
 	go startProcessor(workToDo, &finishedWorkMap)
 	setupWebInterface(workToDo, &finishedWorkMap)
 }
